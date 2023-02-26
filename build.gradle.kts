e72ab64c4a9b8f1fc634aea09120de91b09c9a35
Gabriel Brand <gabr.brand@gmail.com>
2023-02-26 13:39:58 +0100

    Update kotlin + compose version Add description, copyright + license to package (build.gradle.kts)

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,13 @@ compose.desktop {
             targetFormats(TargetFormat.Rpm, TargetFormat.Deb)
             packageName = "Tic-Tac-Toe"
             packageVersion = "2.3.0"
+            description = "A simple Tic-Tac-Toe game written in Compose for Desktop."
+            copyright = "© 2023 Gabriel Brand. All rights reserved."
+            licenseFile.set(project.file("LICENSE"))
+
+//            linux {
+//                iconFile.set(project.file("icon.png"))
+//            }
         }
     }
 }
