4d74f7b41b4e9a8073b792bebb4d690766399d96
Gabriel Brand <gabr.brand@gmail.com>
2023-03-02 17:56:15 +0100

    Update version & add rpm license type

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.example"
-version = "2.3.0"
+version = "2.4.0"
 
 repositories {
     google()
@@ -36,14 +36,14 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Rpm, TargetFormat.Deb)
             packageName = "Tic-Tac-Toe"
-            packageVersion = "2.3.0"
+            packageVersion = "2.4.0"
             description = "A simple Tic-Tac-Toe game written in Compose for Desktop."
             copyright = "© 2023 Gabriel Brand. All rights reserved."
             licenseFile.set(project.file("LICENSE"))
 
-//            linux {
-//                iconFile.set(project.file("icon.png"))
-//            }
+            linux {
+                rpmLicenseType = "GPLv3"
+            }
         }
     }
 }
